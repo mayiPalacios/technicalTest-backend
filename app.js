0af1cae8ff3b2763923e34d206d76ad1040df52a
mayiPalacios <00018619@uca.edu.sv>
2023-07-29 10:55:56 -0600

    :camera: add Cloudinary for file images and :bug: fix the endpoint for add a new movie

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 var cors = require("cors");
-const moviesController = require('./controllers/moviesController');
-const optionsController = require('./controllers/optionsController');
+const moviesController = require("./controllers/moviesController");
+const optionsController = require("./controllers/optionsController");
 const app = express();
-const port = 3010; 
+
+const port = 3010;
 
 const corsOpts = {
-  origin: '*',
-  methods: [
-    'GET',
-    'POST',
-  ],
-  allowedHeaders: [
-    'Content-Type',
-  ],
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
 };
 
 app.use(cors(corsOpts));
-
 app.use(express.json());
 
+const fileupload = require("express-fileupload");
+app.use(
+  fileupload({
+    useTempFiles: true,
+  })
+);
+
 // Rutas para películas
-app.post('/api/movies', moviesController.createMovie);
+app.post("/api/movies", moviesController.createMovie);
 
 // Rutas para opciones
-app.get('/api/options', optionsController.getOptions);
+app.get("/api/options", optionsController.getOptions);
 
 app.listen(port, () => {
   console.log(`API iniciada en http://localhost:${port}`);
--- models/movies.js
+++ models/movies.js
@@ -1,11 +1,22 @@
-const pool = require('../db');
+const pool = require("../db");
 
-const createMovie = async (movie) => {
+const createMovie = async (movie, img) => {
   const { name, budget, date, duration } = movie;
-  const query = 'INSERT INTO movie (name, budget, date, duration) VALUES ($1, $2, $3, $4) RETURNING *';
-  const values = [name, budget, date, duration];
-  const { rows } = await pool.query(query, values);
-  return rows[0];
+  console.log(name);
+
+  // Utilizar marcadores de posición en la consulta SQL
+  const query = `INSERT INTO movie (id, name, budget, date, duration, secure__url_img) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+
+  // Pasar los valores como un array en el orden correspondiente a los marcadores de posición
+  const values = [12, name, parseInt(budget), date, parseInt(duration), img];
+
+  try {
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  } catch (error) {
+    console.error("Error al insertar la película:", error);
+    throw error;
+  }
 };
 
 module.exports = {
