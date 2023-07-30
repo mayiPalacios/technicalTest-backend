559a238902f50ce1350893fea9c91df1f0ef5d87
mayiPalacios <00018619@uca.edu.sv>
2023-07-29 18:48:44 -0600

    :bug: fix script id

--- models/movies.js
+++ models/movies.js
@@ -1,14 +1,14 @@
 const pool = require("../db");
 
 const createMovie = async (movie, img) => {
-  const { name, budget, date, duration } = movie;
+  const { id, name, budget, date, duration } = movie;
   console.log(name);
 
   // Utilizar marcadores de posición en la consulta SQL
   const query = `INSERT INTO movie (id, name, budget, date, duration, secure__url_img) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
 
   // Pasar los valores como un array en el orden correspondiente a los marcadores de posición
-  const values = [12, name, parseInt(budget), date, parseInt(duration), img];
+  const values = [id, name, parseInt(budget), date, parseInt(duration), img];
 
   try {
     const { rows } = await pool.query(query, values);
